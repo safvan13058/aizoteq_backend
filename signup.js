ce5954fc95fea7307daa8996f2b80772380bf22a
safvan13058 <safvan13473@gmail.com>
2024-12-28 20:16:17 +0530

    c

--- signup.js
+++ signup.js
@@ -76,13 +76,18 @@ async function handleSignup(req, res, role) {
         console.log('workingsss')
 
         await db.query(query, values);
+        console.log('db working')
 
         req.session.username = userName; // Store username in session
+        console.log('session')
 
         res.status(201).json({
+            
             message: 'User signed up successfully',
             userSub: jwtsub,
         });
+        console.log(jwtsub)
+        console.log('all working')
     } catch (err) {
         console.error('Cognito sign-up error:', err.message, err.code);
         res.status(500).json({ message: 'Error during Cognito sign-up', error: err.message });
