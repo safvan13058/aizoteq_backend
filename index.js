8b07d5555837ca2e9877e649c1d13ede3f5b1609
safvan13058 <safvan13473@gmail.com>
2025-01-01 12:55:15 +0530

    change

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ async function validateJwt(req, res, next) {
                         'SELECT * FROM users WHERE jwtsub = $1',
                         [userSub]
                     );
-
+                    console.log(result)
                     if (result.rows.length === 0) {
                         return res.status(404).json({ message: 'User not found' });
                     }
