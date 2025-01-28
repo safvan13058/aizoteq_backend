9f1ed4fea839b11d2dff42f26c585d11cff2cc20
safvan13058 <safvan13473@gmail.com>
2025-01-28 21:06:15 +0530

    change

--- MQTT/mqtt.js
+++ MQTT/mqtt.js
@@ -46,7 +46,7 @@ const getDeviceStatus = async (deviceId) => {
   try {
     const data = await iotData.getThingShadow(params).promise();
     const shadow = JSON.parse(data.payload);
-    console.log(`shadow${shadow}`)
+    console.log("Full Shadow Document:", JSON.stringify(shadow, null, 2));
     console.log(`Device ${deviceId} Status:`, shadow.state.reported);
   } catch (err) {
     console.error(`Error fetching status for device ${deviceId}:`, err);
