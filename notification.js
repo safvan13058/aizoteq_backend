62965e8a9831d0c76ce78c17c800a7f973e7a202
safvan13058 <safvan13473@gmail.com>
2025-01-30 05:08:38 +0530

    change

--- notification.js
+++ notification.js
@@ -5,10 +5,24 @@ const nodemailer = require("nodemailer");
 const admin = require('firebase-admin');
 
 // Initialize Firebase Admin SDK
-// const serviceAccount = require('./certificate/aizoteq-c0002-firebase-adminsdk-9fpfe-dfd9cc93a9.json'); // Replace with your Firebase JSON file
+var admin = require("firebase-admin");
+
+const serviceAccount = {
+    type: "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/your-client-email%40your-project-id.iam.gserviceaccount.com"
+  };
+  
 admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(fs.readFileSync('./certificate/aizoteq-c0002-firebase-adminsdk-9fpfe-dfd9cc93a9.json')))
-  });
+  credential: admin.credential.cert(serviceAccount)
+});
 
 // Configure PostgreSQL client for LISTEN/NOTIFY
 const client = new Client({
