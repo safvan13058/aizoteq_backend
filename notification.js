386396080f4d9e95c39582abffcad30f20e27221
safvan13058 <safvan13473@gmail.com>
2025-02-01 03:44:32 +0530

    change

--- notification.js
+++ notification.js
@@ -16,7 +16,7 @@ const serviceAccount = {
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/your-client-email%40your-project-id.iam.gserviceaccount.com"
+    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-9fpfe@aizoteq-c0002.iam.gserviceaccount.com%40aizoteq-c0002.iam.gserviceaccount.com"
   };
   
 admin.initializeApp({
