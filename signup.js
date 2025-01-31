a4fc0d0d15d1772992a8a12794abac6cff8e3478
safvan13058 <safvan13473@gmail.com>
2025-01-31 17:11:22 +0530

    change

--- signup.js
+++ signup.js
@@ -65,8 +65,8 @@ async function handleSignup(req, res, role) {
     };
 
     try {
-        console.log(userName, password, email, phoneNumber, fullName)
-        console.log("signup working")
+        // console.log(userName, password, email, phoneNumber, fullName)
+        // console.log("signup working")
         // Sign up the user in Cognito
         const signUpResponse = await cognito.signUp(params).promise();
         const jwtsub = signUpResponse.UserSub; // The unique identifier for the user in Cognito
@@ -84,7 +84,7 @@ async function handleSignup(req, res, role) {
         console.log(req.session.username)
         console.log(req.session)
         console.log(req.session)
-        console.log("signup working alll")
+        // console.log("signup working alll")
         req.session.jwtsub =jwtsub;
         req.session.role =role;
         res.status(201).json({ 
