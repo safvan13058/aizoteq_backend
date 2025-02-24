d23b98f36d96365a68509a328e96e95867003eb1
safvan13058 <safvan13473@gmail.com>
2025-02-25 04:13:43 +0530

    change

--- login.js
+++ login.js
@@ -170,11 +170,11 @@ login.post('/refresh-token', async (req, res) => {
     console.log(`refresh::${secretHash}`)
     const params = {
         AuthFlow:'REFRESH_TOKEN_AUTH',
-        ClientId:"2h3nid8ifjsojo4hsk96me5ntt",
+        ClientId:process.env.clientId,
         AuthParameters: {
             REFRESH_TOKEN:refreshToken,
             USERNAME:username, // REQUIRED for SECRET_HASH to match
-            // SECRET_HASH:req.cookies.SecretHash
+            SECRET_HASH:req.cookies.SecretHash
         },
     };
 
@@ -308,11 +308,11 @@ login.post('/auth', async (req, res) => {
 
         const loginParams = {
             AuthFlow: 'USER_PASSWORD_AUTH',
-            ClientId: "2h3nid8ifjsojo4hsk96me5ntt",
+            ClientId: process.env.clientId,
             AuthParameters: {
                 USERNAME: username,
                 PASSWORD: password,
-                // SECRET_HASH: secretHash,
+                SECRET_HASH: secretHash,
             },
         };
 
@@ -350,11 +350,11 @@ login.post('/auth', async (req, res) => {
 
         const refreshParams = {
             AuthFlow: 'REFRESH_TOKEN_AUTH',
-            ClientId: process.env.clientId,
+            ClientId: "2h3nid8ifjsojo4hsk96me5ntt",
             AuthParameters: {
                 REFRESH_TOKEN: loginRefreshToken,
                 USERNAME: username,
-                SECRET_HASH: calculateSecretHash(username),
+                // SECRET_HASH: calculateSecretHash(username),
             },
         };
 
