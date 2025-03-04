4667971d789ac904de2005252cf52e5cbe4f6a07
safvan13058 <safvan13473@gmail.com>
2025-03-04 16:54:33 +0530

    change

--- middlewares/s3.js
+++ middlewares/s3.js
@@ -74,7 +74,7 @@ const fileFilter = (req, file, cb) => {
 // Multer upload middleware with renamed instance
 const estimate = multer({
     storage: storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5MB
+    limits: { fileSize: 10 * 1024 * 1024 }, // Limit file size to 5MB
     fileFilter: fileFilter
 });
 
