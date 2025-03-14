ed4088b19f4574445dce01f08bbb49f35368f196
safvan13058 <safvan13473@gmail.com>
2025-03-14 13:15:10 +0530

    change

--- index.js
+++ index.js
@@ -151,8 +151,8 @@ try {
     //     cert: fs.readFileSync('/etc/letsencrypt/live/auslandenglish.com/fullchain.pem'),
     // };
     const options = {
-        key: fs.readFileSync('/etc/letsencrypt/live/admin.aizoteq.com/privkey.pem'),
-        cert: fs.readFileSync('/etc/letsencrypt/live/admin.aizoteq.com/fullchain.pem'),
+        key: fs.readFileSync('/etc/letsencrypt/live/api.aizoteq.com/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/api.aizoteq.com/fullchain.pem'),
     };
 
     // If SSL certificates are available, start the HTTPS server
