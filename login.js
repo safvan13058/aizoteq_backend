d3e0b8eef706b0aa3b540af283a4ec2bc6705ff8
safvan13058 <safvan13473@gmail.com>
2025-03-16 14:39:48 +0530

    change

--- login.js
+++ login.js
@@ -369,8 +369,7 @@ login.post('/auth', async (req, res) => {
             AuthFlow: 'REFRESH_TOKEN_AUTH',
             ClientId: "52ml9t6v1s6esabq5j03vohkl6",
             AuthParameters: {
-                REFRESH_TOKEN: loginRefreshToken,
-                grant_type: "refresh_token"
+                REFRESH_TOKEN: loginRefreshToken.trim(),
             },
         };
 
